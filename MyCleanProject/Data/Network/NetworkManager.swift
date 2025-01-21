782778bf3b781f49f367634678054d710e084f38
sh990920 <sh990920@gmail.com>
2025-01-21 13:28:19 +0900

    클린아키텍처&MVVM 6,7강 강의 코드

--- MyCleanProject/Data/Network/NetworkManager.swift
+++ MyCleanProject/Data/Network/NetworkManager.swift
@@ -2,7 +2,48 @@
 //  NetworkManager.swift
 //  MyCleanProject
 //
-//  Created by 박승환 on 1/20/25.
+//  Created by 박승환 on 1/21/25.
 //
 
 import Foundation
+import Alamofire
+
+
+protocol NetworkManagerProtocol {
+    func fetchData<T: Decodable>(url: String, method: HTTPMethod, parameters: Parameters?) async -> Result<T, NetworkError>
+}
+
+public class NetworkManager {
+    private let session: SessionProtocol
+    init(session: SessionProtocol) {
+        self.session = session
+    }
+    
+    private let tokenHeader: HTTPHeaders = {
+        let tokenHeader = HTTPHeader(name: "Authorization", value: "Bearer ApiKey")
+        
+        return HTTPHeaders([tokenHeader])
+    }()
+    
+    func fetchData<T: Decodable>(url: String, method: HTTPMethod, parameters: Parameters?) async -> Result<T, NetworkError> {
+        guard let url = URL(string: url) else {
+            return .failure(.urlError)
+        }
+        let result = await session.request(url, method: method, parameters: parameters, headers: tokenHeader).serializingData().response
+        if let error = result.error { return .failure(.requestFailed(error.localizedDescription))}
+        guard let data = result.data else { return .failure(.dataNil)}
+        guard let response = result.response else { return .failure(.invalidResponse)}
+        if 200..<400 ~= response.statusCode {
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                return .success(decodedData)
+            } catch {
+                return .failure(.failToDecode(error.localizedDescription))
+            }
+            
+        } else {
+            return .failure(.serverError(response.statusCode))
+        }
+    }
+    
+}
--- MyCleanProject/Data/Network/UserSession.swift
+++ MyCleanProject/Data/Network/UserSession.swift
@@ -2,7 +2,33 @@
 //  UserSession.swift
 //  MyCleanProject
 //
-//  Created by 박승환 on 1/20/25.
+//  Created by 박승환 on 1/21/25.
 //
 
 import Foundation
+import Alamofire
+
+//네트워크 호출 테스트코드 MockSession 추상화
+
+public protocol SessionProtocol {
+    func request(_ convertible: URLConvertible,
+                 method: HTTPMethod,
+                 parameters: Parameters?,
+                 headers: HTTPHeaders?) -> DataRequest
+}
+
+class UserSession: SessionProtocol {
+    private var session: Session
+    init() {
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        self.session = Session(configuration: config)
+    }
+    
+    func request(_ convertible: URLConvertible,
+                 method: HTTPMethod = .get,
+                 parameters: Parameters? = nil,
+                 headers: HTTPHeaders? = nil) -> DataRequest {
+        return session.request(convertible, method: method, parameters: parameters, headers: headers)
+    }
+}
